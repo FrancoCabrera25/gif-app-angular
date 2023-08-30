a4192fa54f096016943a5f8850dd767e1a90ccfb
Franco Cabrera <cabrera.franco@outlook.com>
2023-08-29 23:42:20 -0300

    add search box

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
 
 @Component({
-  selector: 'app-search-box',
-  templateUrl: './search-box.component.html',
-  styleUrls: ['./search-box.component.scss']
+    selector: 'app-search-box',
+    templateUrl: './search-box.component.html',
+    styleUrls: ['./search-box.component.scss'],
 })
 export class SearchBoxComponent {
+    @ViewChild('txtTagInput') tagInput!: ElementRef<HTMLInputElement>;
 
+    constructor(private gifsService: GifsService) {}
+
+    searchTag(): void {
+        const newTag = this.tagInput.nativeElement.value;
+
+        this.gifsService.searchTag(newTag);
+
+        this.tagInput.nativeElement.value = '';
+    }
 }
